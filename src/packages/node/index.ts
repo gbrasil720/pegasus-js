d249d23e180160e34adab9db6fb5198379fd3814
GUILHERME BRASIL <dev.guilhermebrasil@gmail.com>
2022-04-10 19:14:23 -0300

    :wrench: export: add default exports for validators & generators

--- src/packages/node/index.ts
+++ src/packages/node/index.ts
@@ -1,2 +1,4 @@
 export { default as LogProvider } from './logs/LogProvider'
 export { default as Logger } from './logger/Logger'
+export { default as Generators } from './generators/Generators'
+export { default as Validators } from './validators/Validators'
--- src/packages/node/validators/Validators.ts
+++ src/packages/node/validators/Validators.ts
@@ -1,7 +1,7 @@
 import { Utils } from '../../../utils'
 import { NodeLib } from '../../../types/node/node'
 
-export class Validators extends Utils {
+export default class Validators extends Utils {
   public isValidEmail(email: string): boolean {
     const re =
       /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
