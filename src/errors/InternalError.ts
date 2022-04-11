ca9a2dcf95360ce462eee5865acd7c28bcc54b33
GUILHERME BRASIL <dev.guilhermebrasil@gmail.com>
2022-04-10 23:11:24 -0300

    :bug: fix: fix internal error and package.json

--- src/errors/InternalError.ts
+++ src/errors/InternalError.ts
@@ -1,7 +1,8 @@
 import chalk from 'chalk'
-import logSymbols from 'log-symbols'
 
-const text = chalk.reset.inverse.bold.red(` ${logSymbols.error} error `)
+import { ICONS } from '../utils/icons'
+
+const text = chalk.reset.inverse.bold.red(` ${ICONS.failed} error `)
 
 export class InternalError extends Error {
   constructor(message: string) {
